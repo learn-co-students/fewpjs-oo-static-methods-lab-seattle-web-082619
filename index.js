bca90e6010451b3d4e5681aecbd9bdd1c703d8d5
Kara Odle <karajo.odle@gmail.coml>
2019-10-16 12:40:52 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]/g, '')
+  }
+
+  static titleize(string) {
+    let words_keep = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let array = string.split(' ')
+    let result = []
+    array.forEach(word => {
+      if (word === array[0] || !words_keep.includes(word)) {
+        result.push(word[0].toUpperCase() + word.slice(1))
+      }
+      else {
+        result.push(word)
+      }
+    })
+    return result.join(" ")
+  }
 }
